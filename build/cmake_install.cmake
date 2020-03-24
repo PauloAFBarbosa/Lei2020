4d78df9ba77ba7b4eddece9ae78f088c1c496ee1
Paulo Barbosa <paulob1992@hotmail.com>
2020-03-24 19:31:54 +0000

    alterado cmakelist

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/paulo/OneDrive/Ambiente de Trabalho/Escola/LEI
+# Install script for directory: C:/Users/paulo/OneDrive/Ambiente de Trabalho/Escola/LEI/Lei2020
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/paulo/OneDrive/Ambiente de Trabalho/Escola/LEI/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/paulo/OneDrive/Ambiente de Trabalho/Escola/LEI/Lei2020/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
